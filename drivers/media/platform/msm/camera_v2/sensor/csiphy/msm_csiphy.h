f5ab9555a8f767fcc7dd70d3beeed88c9779afe2
VijayaKumar T M <vtmuni@codeaurora.org>
2017-05-03 15:36:43 +0530

    msm: camera: Add regulator enable and disable independent of CSID
    Regulator enable and disable of CSIPHY depends on the CSID module.
    Make the enable and disable of clk regulator independent of CSIPHY.
    
    CRs-Fixed: 1107702
    Change-Id: Iabb5eb28d63b34a4c3201c53be17054a1907f4fe
    Signed-off-by: Ravi Kishore Tanuku <rktanuku@codeaurora.org>
    Signed-off-by: VijayaKumar T M <vtmuni@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/sensor/csiphy/msm_csiphy.h
+++ drivers/media/platform/msm/camera_v2/sensor/csiphy/msm_csiphy.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2014, 2017 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -142,7 +142,11 @@ struct csiphy_device {
 	struct platform_device *pdev;
 	struct msm_sd_subdev msm_sd;
 	struct v4l2_subdev subdev;
+	struct resource *mem;
+	struct resource *clk_mux_mem;
 	struct resource *irq;
+	struct resource *io;
+	struct resource *clk_mux_io;
 	void __iomem *base;
 	void __iomem *clk_mux_base;
 	struct mutex mutex;
